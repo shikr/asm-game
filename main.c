f882e62c7b2318eaecad98c35051b5cdfaf224d7
Cristobal Rodriguez <cristobal.rod0907@gmail.com>
2026-05-14 11:09:42 -0600

    initial commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#define TB_IMPL
+#include "termbox2/termbox2.h"
+
+int main() {
+  struct tb_event ev;
+  int y = 0;
+
+  tb_init();
+
+  tb_printf(0, y++, TB_GREEN, 0, "hello from termbox");
+  tb_printf(0, y++, 0, 0, "width=%d height=%d", tb_width(), tb_height());
+  tb_printf(0, y++, 0, 0, "press any key...");
+  tb_present();
+
+  tb_poll_event(&ev);
+
+  tb_printf(0, y, 0, 0, "event type=%d key=%d ch=%c", ev.type, ev.key, ev.ch);
+  tb_printf(0, y - 1, 0, 0, "press any key to quit...");
+  tb_present();
+
+  tb_poll_event(&ev);
+  tb_shutdown();
+
+  return 0;
+}
